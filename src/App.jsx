3f00c01fee5c8284cbe08bbe50188b568c29d45c
RahulQurbanov <156121142+RahulQurbanov@users.noreply.github.com>
2025-04-04 18:49:42 +0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 export default function App() {
   return (
-    <div className="bg-green-100">Portfolio</div>
+    <div className="bg-green-100">Portfolio:
+  </div>
   )
 }
