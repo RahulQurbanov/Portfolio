569b8e357701c97e0e66477576b04c165dd95386
Rahul Qurbanov <rahulqurbanov055@gmail.com>
2025-04-18 20:26:37 +0400

    Projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
 export default function Projects() {
-    return <div>
-        <h1>You can check out all my projects on my <a href="#">GitHub</a> Profile</h1>
+    return <div className='bg-[#0a192f]'>
+        <div className='w-[65%] m-auto'>
+        <h1 className='text-[29px] font-bold w-[76%] m-auto text-white pt-[45px]'>You can check out all my projects on my <a href="https://github.com/RahulQurbanov">GitHub</a> Profile</h1>
+        </div>
     </div>
 }
