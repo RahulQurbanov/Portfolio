1e7318526862c734e6be2dbc7c97210629e67a35
Rahul Qurbanov <rahulqurbanov055@gmail.com>
2025-04-19 18:34:45 +0400

    Second Porject is finish

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,9 +4,9 @@ export default function Projects() {
     return <div className='bg-[#0a192f]'>
         <div className='w-[75%] m-auto'>
         <h1 className='text-[29px] font-bold w-[76%] m-auto text-white py-[45px]'>You can check out all my projects on my <a href="https://github.com/RahulQurbanov">GitHub</a> Profile</h1>
-        <div className='flex flex-row gap-[20px] justify-around'>
+        <div className='flex flex-row gap-[20px] justify-around mb-[125px]'>
             <div className='w-[50%]'>
-                <img src="src/assets/img/Weather.png" className='h-full opacity-20 hover:opacity-65 cursor-pointer transition-all translate-0.5 ease-in-out'/>
+            <a href="https://github.com/RahulQurbanov/Weather-prognoz"><img src="src/assets/img/Weather.png" className='h-full opacity-20 hover:opacity-65 cursor-pointer transition-all translate-0.5 ease-in-out'/></a>
             </div>
             <div className='w-[45%] flex flex-col gap-[15px]'>
                <h2 className='text-[#ccd6f6] text-[30px] font-bold'>Weather Projects</h2>
@@ -21,6 +21,24 @@ export default function Projects() {
            <a href="https://github.com/RahulQurbanov/Weather-prognoz"><img src="./src/assets/img/github1.svg" className="w-[50px] h-[50px] cursor-pointer  opacity-45 hover:opacity-100" /></a>
             <a href="https://rahulqurbanov.github.io/Weather-prognoz/"><img src="./src/assets/img/live.svg" className="w-[40px] h-[40px] cursor-pointer opacity-45 hover:opacity-100" /></a>
             </div>
+            </div>
+        </div>
+        <div className='flex flex-row gap-[20px] justify-around mb-[125px]'>
+            <div className='w-[45%] flex flex-col gap-[15px]'>
+               <h2 className='text-[#ccd6f6] text-[30px] font-bold'>KoolCoders Projects</h2>
+               <div className='w-[500px] bg-[#112240] text-gray-300 rounded-lg py-2 px-5'>This project is designed as an online course platform. It was fully built using HTML, CSS, and JavaScript. The website layout is clean and responsive, featuring sections for courses, instructors, and interactive elements to simulate a real e-learning experience.</div>
+            <div className='flex gap-5'>
+            <img src="./src/assets/img/html.webp" className="w-[35px] h-[35px]" />
+                  <img src="./src/assets/img/css.svg" className="w-[35px] h-[35px]" />
+                  <img src="./src/assets/img/js.png" className="w-[35px] h-[35px]" />
+            </div>
+            <div className='flex gap-2 items-end justify-end'>
+           <a href="https://github.com/RahulQurbanov/Project-KoolCoders"><img src="./src/assets/img/github1.svg" className="w-[50px] h-[50px] cursor-pointer  opacity-45 hover:opacity-100" /></a>
+            <a href="https://rahulqurbanov.github.io/Project-KoolCoders/"><img src="./src/assets/img/live.svg" className="w-[40px] h-[40px] cursor-pointer opacity-45 hover:opacity-100" /></a>
+            </div>
+            </div>
+            <div className='w-[50%]'>
+            <a href="https://github.com/RahulQurbanov/Project-KoolCoders"><img src="src/assets/img/KoolCoders.png" className='h-[95%] opacity-20 hover:opacity-65 cursor-pointer transition-all translate-0.5 ease-in-out'/></a>
             </div>
         </div>
         </div>
