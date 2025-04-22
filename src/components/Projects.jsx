74972aa09d9ff63a9ca10deec1282a1763b6397c
Rahul Qurbanov <rahulqurbanov055@gmail.com>
2025-04-22 18:23:33 +0400

    Change

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,6 +4,71 @@ export default function Projects() {
     return <div className='bg-[#0a192f]'>
         <div className='w-[75%] m-auto'>
         <h1 className='text-[29px] font-bold w-[76%] m-auto text-white py-[45px]'>You can check out all my projects on my <a href="https://github.com/RahulQurbanov">GitHub</a> Profile</h1>
+        <div className='flex flex-row gap-[20px] justify-around mb-[125px'>
+            <div className='w-[50%]'>
+            <a href="https://github.com/RahulQurbanov/Investment-Calculator"><img src="src/assets/img/investment.png" className='h-[75%] w-[95%] opacity-20 hover:opacity-65 cursor-pointer transition-all translate-0.5 ease-in-out'/></a>
+            </div>
+            <div className='w-[45%] flex flex-col gap-[15px]'>
+               <h2 className='text-[#ccd6f6] text-[30px] font-bold'>Investment Calculator</h2>
+               <div className='w-[500px] bg-[#112240] text-gray-300 rounded-lg py-2 px-5'>This project is an interactive Investment Calculator built with React and styled using Tailwind CSS.
+Users can input their desired investment amount, interest rate, and the number of months — the calculator will automatically compute the potential returns based on these values.</div>
+            <div className='flex gap-5'>
+                  <img src="./src/assets/img/html.webp" className="w-[35px] h-[35px]" />
+                  <img src="./src/assets/img/js.png" className="w-[35px] h-[35px]" />
+                  <img src="./src/assets/img/react.svg" className="w-[35px] h-[35px]" />
+                  <img src="./src/assets/img/vite.svg" className="w-[35px] h-[35px]" />
+                  <img src="./src/assets/img/tailwind.svg" className="w-[35px] h-[35px]" />
+                  <img src="./src/assets/img/api.svg" className="w-[35px] h-[35px]" />
+            </div>
+            <div className='flex gap-2 items-end justify-end'>
+           <a href="https://github.com/RahulQurbanov/Investment-Calculator"><img src="./src/assets/img/github1.svg" className="w-[50px] h-[50px] cursor-pointer  opacity-45 hover:opacity-100" /></a>
+            <a href=""><img src="./src/assets/img/live.svg" className="w-[40px] h-[40px] cursor-pointer opacity-45 hover:opacity-100" /></a>
+            </div>
+            </div>
+        </div>
+        <div className='flex flex-row gap-[20px] justify-around mb-[125px'>
+            <div className='w-[50%]'>
+            <a href="https://github.com/RahulQurbanov/Investment-Calculator"><img src="src/assets/img/investment.png" className='h-[75%] w-[95%] opacity-20 hover:opacity-65 cursor-pointer transition-all translate-0.5 ease-in-out'/></a>
+            </div>
+            <div className='w-[45%] flex flex-col gap-[15px]'>
+               <h2 className='text-[#ccd6f6] text-[30px] font-bold'>Investment Calculator</h2>
+               <div className='w-[500px] bg-[#112240] text-gray-300 rounded-lg py-2 px-5'>This project is an interactive Investment Calculator built with React and styled using Tailwind CSS.
+Users can input their desired investment amount, interest rate, and the number of months — the calculator will automatically compute the potential returns based on these values.</div>
+            <div className='flex gap-5'>
+                  <img src="./src/assets/img/html.webp" className="w-[35px] h-[35px]" />
+                  <img src="./src/assets/img/js.png" className="w-[35px] h-[35px]" />
+                  <img src="./src/assets/img/react.svg" className="w-[35px] h-[35px]" />
+                  <img src="./src/assets/img/vite.svg" className="w-[35px] h-[35px]" />
+                  <img src="./src/assets/img/tailwind.svg" className="w-[35px] h-[35px]" />
+                  <img src="./src/assets/img/api.svg" className="w-[35px] h-[35px]" />
+            </div>
+            <div className='flex gap-2 items-end justify-end'>
+           <a href="https://github.com/RahulQurbanov/Investment-Calculator"><img src="./src/assets/img/github1.svg" className="w-[50px] h-[50px] cursor-pointer  opacity-45 hover:opacity-100" /></a>
+            <a href=""><img src="./src/assets/img/live.svg" className="w-[40px] h-[40px] cursor-pointer opacity-45 hover:opacity-100" /></a>
+            </div>
+            </div>
+        </div>
+        <div className='flex flex-row gap-[20px] justify-around mb-[125px]'>
+            <div className='w-[45%] flex flex-col gap-[15px]'>
+               <h2 className='text-[#ccd6f6] text-[30px] font-bold'>Quiz App</h2>
+               <div className='w-[500px] bg-[#112240] text-gray-300 rounded-lg py-2 px-5'>This is a dynamic Quiz App built using React and styled with Tailwind CSS.
+                All questions are loaded from a JSON file, and users receive points for each correct answer, while wrong answers do not reduce the score. The app offers smooth interaction, real-time feedback, and is perfect for quick knowledge testing.</div>
+            <div className='flex gap-5'>
+                  <img src="./src/assets/img/html.webp" className="w-[35px] h-[35px]" />
+                  <img src="./src/assets/img/js.png" className="w-[35px] h-[35px]" />
+                  <img src="./src/assets/img/react.svg" className="w-[35px] h-[35px]" />
+                  <img src="./src/assets/img/vite.svg" className="w-[35px] h-[35px]" />
+                  <img src="./src/assets/img/tailwind.svg" className="w-[35px] h-[35px]" />
+            </div>
+            <div className='flex gap-2 items-end justify-end'>
+           <a href="https://github.com/RahulQurbanov/Quiz-App"><img src="./src/assets/img/github1.svg" className="w-[50px] h-[50px] cursor-pointer  opacity-45 hover:opacity-100" /></a>
+            <a href="http://localhost:5173/"><img src="./src/assets/img/live.svg" className="w-[40px] h-[40px] cursor-pointer opacity-45 hover:opacity-100" /></a>
+            </div>
+            </div>
+            <div className='w-[50%]'>
+            <a href="https://github.com/RahulQurbanov/Quiz-App"><img src="src/assets/img/quizapp.png" className='h-[95%] opacity-20 hover:opacity-65 cursor-pointer transition-all translate-0.5 ease-in-out'/></a>
+            </div>
+        </div>
         <div className='flex flex-row gap-[20px] justify-around mb-[125px]'>
             <div className='w-[50%]'>
             <a href="https://github.com/RahulQurbanov/Weather-prognoz"><img src="src/assets/img/Weather.png" className='h-full opacity-20 hover:opacity-65 cursor-pointer transition-all translate-0.5 ease-in-out'/></a>
@@ -41,7 +106,7 @@ export default function Projects() {
             <a href="https://github.com/RahulQurbanov/Project-KoolCoders"><img src="src/assets/img/KoolCoders.png" className='h-[95%] opacity-20 hover:opacity-65 cursor-pointer transition-all translate-0.5 ease-in-out'/></a>
             </div>
         </div>
-        <div className='flex flex-row gap-[20px] justify-around mb-[125px]'>
+        <div className='flex flex-row gap-[20px] justify-around'>
             <div className='w-[50%]'>
             <a href="https://rahulqurbanov.github.io/Todo-List-/"><img src="src/assets/img/todolist.png" className='h-[90%] w-[95%] opacity-20 hover:opacity-65 cursor-pointer transition-all translate-0.5 ease-in-out'/></a>
             </div>
@@ -57,71 +122,6 @@ export default function Projects() {
             <div className='flex gap-2 items-end justify-end'>
            <a href="https://github.com/RahulQurbanov/Todo-List-"><img src="./src/assets/img/github1.svg" className="w-[50px] h-[50px] cursor-pointer  opacity-45 hover:opacity-100" /></a>
             <a href="https://rahulqurbanov.github.io/Todo-List-/"><img src="./src/assets/img/live.svg" className="w-[40px] h-[40px] cursor-pointer opacity-45 hover:opacity-100" /></a>
-            </div>
-            </div>
-        </div>
-        <div className='flex flex-row gap-[20px] justify-around mb-[125px]'>
-            <div className='w-[45%] flex flex-col gap-[15px]'>
-               <h2 className='text-[#ccd6f6] text-[30px] font-bold'>Quiz App</h2>
-               <div className='w-[500px] bg-[#112240] text-gray-300 rounded-lg py-2 px-5'>This is a dynamic Quiz App built using React and styled with Tailwind CSS.
-                All questions are loaded from a JSON file, and users receive points for each correct answer, while wrong answers do not reduce the score. The app offers smooth interaction, real-time feedback, and is perfect for quick knowledge testing.</div>
-            <div className='flex gap-5'>
-                  <img src="./src/assets/img/html.webp" className="w-[35px] h-[35px]" />
-                  <img src="./src/assets/img/js.png" className="w-[35px] h-[35px]" />
-                  <img src="./src/assets/img/react.svg" className="w-[35px] h-[35px]" />
-                  <img src="./src/assets/img/vite.svg" className="w-[35px] h-[35px]" />
-                  <img src="./src/assets/img/tailwind.svg" className="w-[35px] h-[35px]" />
-            </div>
-            <div className='flex gap-2 items-end justify-end'>
-           <a href="https://github.com/RahulQurbanov/Quiz-App"><img src="./src/assets/img/github1.svg" className="w-[50px] h-[50px] cursor-pointer  opacity-45 hover:opacity-100" /></a>
-            <a href="http://localhost:5173/"><img src="./src/assets/img/live.svg" className="w-[40px] h-[40px] cursor-pointer opacity-45 hover:opacity-100" /></a>
-            </div>
-            </div>
-            <div className='w-[50%]'>
-            <a href="https://github.com/RahulQurbanov/Quiz-App"><img src="src/assets/img/quizapp.png" className='h-[95%] opacity-20 hover:opacity-65 cursor-pointer transition-all translate-0.5 ease-in-out'/></a>
-            </div>
-        </div>
-        <div className='flex flex-row gap-[20px] justify-around mb-[125px'>
-            <div className='w-[50%]'>
-            <a href="https://github.com/RahulQurbanov/Investment-Calculator"><img src="src/assets/img/investment.png" className='h-[75%] w-[95%] opacity-20 hover:opacity-65 cursor-pointer transition-all translate-0.5 ease-in-out'/></a>
-            </div>
-            <div className='w-[45%] flex flex-col gap-[15px]'>
-               <h2 className='text-[#ccd6f6] text-[30px] font-bold'>Investment Calculator</h2>
-               <div className='w-[500px] bg-[#112240] text-gray-300 rounded-lg py-2 px-5'>This project is an interactive Investment Calculator built with React and styled using Tailwind CSS.
-Users can input their desired investment amount, interest rate, and the number of months — the calculator will automatically compute the potential returns based on these values.</div>
-            <div className='flex gap-5'>
-                  <img src="./src/assets/img/html.webp" className="w-[35px] h-[35px]" />
-                  <img src="./src/assets/img/js.png" className="w-[35px] h-[35px]" />
-                  <img src="./src/assets/img/react.svg" className="w-[35px] h-[35px]" />
-                  <img src="./src/assets/img/vite.svg" className="w-[35px] h-[35px]" />
-                  <img src="./src/assets/img/tailwind.svg" className="w-[35px] h-[35px]" />
-                  <img src="./src/assets/img/api.svg" className="w-[35px] h-[35px]" />
-            </div>
-            <div className='flex gap-2 items-end justify-end'>
-           <a href="https://github.com/RahulQurbanov/Investment-Calculator"><img src="./src/assets/img/github1.svg" className="w-[50px] h-[50px] cursor-pointer  opacity-45 hover:opacity-100" /></a>
-            <a href=""><img src="./src/assets/img/live.svg" className="w-[40px] h-[40px] cursor-pointer opacity-45 hover:opacity-100" /></a>
-            </div>
-            </div>
-        </div>
-        <div className='flex flex-row gap-[20px] justify-around mb-[125px'>
-            <div className='w-[50%]'>
-            <a href="https://github.com/RahulQurbanov/Investment-Calculator"><img src="src/assets/img/investment.png" className='h-[75%] w-[95%] opacity-20 hover:opacity-65 cursor-pointer transition-all translate-0.5 ease-in-out'/></a>
-            </div>
-            <div className='w-[45%] flex flex-col gap-[15px]'>
-               <h2 className='text-[#ccd6f6] text-[30px] font-bold'>Investment Calculator</h2>
-               <div className='w-[500px] bg-[#112240] text-gray-300 rounded-lg py-2 px-5'>This project is an interactive Investment Calculator built with React and styled using Tailwind CSS.
-Users can input their desired investment amount, interest rate, and the number of months — the calculator will automatically compute the potential returns based on these values.</div>
-            <div className='flex gap-5'>
-                  <img src="./src/assets/img/html.webp" className="w-[35px] h-[35px]" />
-                  <img src="./src/assets/img/js.png" className="w-[35px] h-[35px]" />
-                  <img src="./src/assets/img/react.svg" className="w-[35px] h-[35px]" />
-                  <img src="./src/assets/img/vite.svg" className="w-[35px] h-[35px]" />
-                  <img src="./src/assets/img/tailwind.svg" className="w-[35px] h-[35px]" />
-                  <img src="./src/assets/img/api.svg" className="w-[35px] h-[35px]" />
-            </div>
-            <div className='flex gap-2 items-end justify-end'>
-           <a href="https://github.com/RahulQurbanov/Investment-Calculator"><img src="./src/assets/img/github1.svg" className="w-[50px] h-[50px] cursor-pointer  opacity-45 hover:opacity-100" /></a>
-            <a href=""><img src="./src/assets/img/live.svg" className="w-[40px] h-[40px] cursor-pointer opacity-45 hover:opacity-100" /></a>
             </div>
             </div>
         </div>
