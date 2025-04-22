eae04b37c2541efb2ba785dcbfd39152ecc3bd03
Rahul Qurbanov <rahulqurbanov055@gmail.com>
2025-04-22 18:36:56 +0400

    Change some projrcts

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -27,9 +27,6 @@ Users can input their desired investment amount, interest rate, and the number o
             </div>
         </div>
         <div className='flex flex-row gap-[20px] justify-around mb-[125px'>
-            <div className='w-[50%]'>
-            <a href="https://github.com/RahulQurbanov/Investment-Calculator"><img src="src/assets/img/investment.png" className='h-[75%] w-[95%] opacity-20 hover:opacity-65 cursor-pointer transition-all translate-0.5 ease-in-out'/></a>
-            </div>
             <div className='w-[45%] flex flex-col gap-[15px]'>
                <h2 className='text-[#ccd6f6] text-[30px] font-bold'>Investment Calculator</h2>
                <div className='w-[500px] bg-[#112240] text-gray-300 rounded-lg py-2 px-5'>This project is an interactive Investment Calculator built with React and styled using Tailwind CSS.
@@ -47,8 +44,14 @@ Users can input their desired investment amount, interest rate, and the number o
             <a href=""><img src="./src/assets/img/live.svg" className="w-[40px] h-[40px] cursor-pointer opacity-45 hover:opacity-100" /></a>
             </div>
             </div>
+            <div className='w-[50%]'>
+            <a href="https://github.com/RahulQurbanov/Investment-Calculator"><img src="src/assets/img/investment.png" className='h-[75%] w-[95%] opacity-20 hover:opacity-65 cursor-pointer transition-all translate-0.5 ease-in-out'/></a>
+            </div>
         </div>
         <div className='flex flex-row gap-[20px] justify-around mb-[125px]'>
+        <div className='w-[50%]'>
+            <a href="https://github.com/RahulQurbanov/Quiz-App"><img src="src/assets/img/quizapp.png" className='h-[95%] opacity-20 hover:opacity-65 cursor-pointer transition-all translate-0.5 ease-in-out'/></a>
+            </div>
             <div className='w-[45%] flex flex-col gap-[15px]'>
                <h2 className='text-[#ccd6f6] text-[30px] font-bold'>Quiz App</h2>
                <div className='w-[500px] bg-[#112240] text-gray-300 rounded-lg py-2 px-5'>This is a dynamic Quiz App built using React and styled with Tailwind CSS.
@@ -65,14 +68,8 @@ Users can input their desired investment amount, interest rate, and the number o
             <a href="http://localhost:5173/"><img src="./src/assets/img/live.svg" className="w-[40px] h-[40px] cursor-pointer opacity-45 hover:opacity-100" /></a>
             </div>
             </div>
-            <div className='w-[50%]'>
-            <a href="https://github.com/RahulQurbanov/Quiz-App"><img src="src/assets/img/quizapp.png" className='h-[95%] opacity-20 hover:opacity-65 cursor-pointer transition-all translate-0.5 ease-in-out'/></a>
-            </div>
         </div>
         <div className='flex flex-row gap-[20px] justify-around mb-[125px]'>
-            <div className='w-[50%]'>
-            <a href="https://github.com/RahulQurbanov/Weather-prognoz"><img src="src/assets/img/Weather.png" className='h-full opacity-20 hover:opacity-65 cursor-pointer transition-all translate-0.5 ease-in-out'/></a>
-            </div>
             <div className='w-[45%] flex flex-col gap-[15px]'>
                <h2 className='text-[#ccd6f6] text-[30px] font-bold'>Weather Projects</h2>
                <div className='w-[500px] bg-[#112240] text-gray-300 rounded-lg py-2 px-5'>This project was built using HTML, CSS, and JavaScript. It fetches real-time 7-day weather forecasts for any city worldwide using the OpenWeather API. The user can search for a city, and the app displays accurate and up-to-date weather information in a clean and user-friendly design.</div>
@@ -87,8 +84,14 @@ Users can input their desired investment amount, interest rate, and the number o
             <a href="https://rahulqurbanov.github.io/Weather-prognoz/"><img src="./src/assets/img/live.svg" className="w-[40px] h-[40px] cursor-pointer opacity-45 hover:opacity-100" /></a>
             </div>
             </div>
+            <div className='w-[50%]'>
+            <a href="https://github.com/RahulQurbanov/Weather-prognoz"><img src="src/assets/img/Weather.png" className='h-full opacity-20 hover:opacity-65 cursor-pointer transition-all translate-0.5 ease-in-out'/></a>
+            </div>
         </div>
         <div className='flex flex-row gap-[20px] justify-around mb-[125px]'>
+        <div className='w-[50%]'>
+            <a href="https://github.com/RahulQurbanov/Project-KoolCoders"><img src="src/assets/img/KoolCoders.png" className='h-[95%] opacity-20 hover:opacity-65 cursor-pointer transition-all translate-0.5 ease-in-out'/></a>
+            </div>
             <div className='w-[45%] flex flex-col gap-[15px]'>
                <h2 className='text-[#ccd6f6] text-[30px] font-bold'>KoolCoders Projects</h2>
                <div className='w-[500px] bg-[#112240] text-gray-300 rounded-lg py-2 px-5'>This project is designed as an online course platform. It was fully built using HTML, CSS, and JavaScript. The website layout is clean and responsive, featuring sections for courses, instructors, and interactive elements to simulate a real e-learning experience.</div>
@@ -102,14 +105,8 @@ Users can input their desired investment amount, interest rate, and the number o
             <a href="https://rahulqurbanov.github.io/Project-KoolCoders/"><img src="./src/assets/img/live.svg" className="w-[40px] h-[40px] cursor-pointer opacity-45 hover:opacity-100" /></a>
             </div>
             </div>
-            <div className='w-[50%]'>
-            <a href="https://github.com/RahulQurbanov/Project-KoolCoders"><img src="src/assets/img/KoolCoders.png" className='h-[95%] opacity-20 hover:opacity-65 cursor-pointer transition-all translate-0.5 ease-in-out'/></a>
-            </div>
         </div>
         <div className='flex flex-row gap-[20px] justify-around'>
-            <div className='w-[50%]'>
-            <a href="https://rahulqurbanov.github.io/Todo-List-/"><img src="src/assets/img/todolist.png" className='h-[90%] w-[95%] opacity-20 hover:opacity-65 cursor-pointer transition-all translate-0.5 ease-in-out'/></a>
-            </div>
             <div className='w-[45%] flex flex-col gap-[15px]'>
                <h2 className='text-[#ccd6f6] text-[30px] font-bold'>TodoList App</h2>
                <div className='w-[500px] bg-[#112240] text-gray-300 rounded-lg py-2 px-5'>This project is a simple and user-friendly To-Do List built with HTML, CSS, and JavaScript.
@@ -123,6 +120,9 @@ Users can input their desired investment amount, interest rate, and the number o
            <a href="https://github.com/RahulQurbanov/Todo-List-"><img src="./src/assets/img/github1.svg" className="w-[50px] h-[50px] cursor-pointer  opacity-45 hover:opacity-100" /></a>
             <a href="https://rahulqurbanov.github.io/Todo-List-/"><img src="./src/assets/img/live.svg" className="w-[40px] h-[40px] cursor-pointer opacity-45 hover:opacity-100" /></a>
             </div>
+            </div>
+            <div className='w-[50%]'>
+            <a href="https://rahulqurbanov.github.io/Todo-List-/"><img src="src/assets/img/todolist.png" className='h-[90%] w-[95%] opacity-20 hover:opacity-65 cursor-pointer transition-all translate-0.5 ease-in-out'/></a>
             </div>
         </div>
         </div>
