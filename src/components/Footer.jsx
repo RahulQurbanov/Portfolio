f57688d729efe16f79f5529a6eb9f72a02c95c2b
Rahul Qurbanov <rahulqurbanov055@gmail.com>
2025-04-24 20:06:51 +0400

    Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,8 +4,8 @@ export default function Footer() {
         <div className="w-[73%] m-auto pt-[75px]">
             <h1 className="text-white text-3xl font-bold">Contact Info :</h1>
             <div className="text-white text-lg mt-[25px]">
-                <p>Email: <span className="text-[#64a7ff]">rahulqurbanov055@gmail.com</span></p>
-                <p>Phone: <span className="text-[#64a7ff]">+9940558902705</span></p>
+                <p>Email: <span className="text-[#1e467a]">rahulqurbanov055@gmail.com</span></p>
+                <p>Phone: <span className="text-[#1e467a]">+9940558902705</span></p>
             </div>
             <div className="flex gap-1 items-center mt-[20px]">
                <a href="https://github.com/RahulQurbanov?tab=overview&from=2025-04-01&to=2025-04-21"><img src="src/assets/img/github.svg" className="w-[40px] h-[30px] cursor-pointer"/></a>
