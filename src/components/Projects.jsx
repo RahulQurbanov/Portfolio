4561a8b4abd81630e497a4caa6f44392be7c2dfd
Rahul Qurbanov <rahulqurbanov055@gmail.com>
2025-04-30 20:29:52 +0400

    Animation

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -150,7 +150,7 @@ export default function Projects() {
           Profile.
         </h1>
         
-        <div className='flex flex-row gap-[20px] justify-around mb-[125px]' ref={projectRef}>
+        <div className='flex flex-row gap-[20px] justify-around mb-[125px' ref={projectRef}>
           <div className={`w-[50%] ${inView ? 'animate__animated animate__fadeInUp' : ''}`}>
             <a href="https://github.com/RahulQurbanov/Investment-Calculator">
               <img src="src/assets/img/investment.png" className='h-[75%] w-[475px] rounded-xl opacity-20 hover:opacity-65 cursor-pointer transition-all translate-0.5 ease-in-out' />
@@ -163,11 +163,47 @@ export default function Projects() {
               This project is an interactive Investment Calculator built with React and styled using Tailwind CSS.
               Users can input their desired investment amount, interest rate, and the number of months — the calculator will automatically compute the potential returns based on these values.
             </div>
-            
           </div>
         </div>
+        <div className='flex flex-row gap-[20px] justify-around'>
+//             <div className='w-[45%] flex flex-col gap-[15px]'>
+//                <h2 className='text-[#ccd6f6] text-[30px] font-bold'>Weather Projects</h2>
+//                <div className='w-[500px] bg-[#112240] text-gray-300 rounded-lg py-2 px-5'>This project was built using HTML, CSS, and JavaScript. It fetches real-time 7-day weather forecasts for any city worldwide using the OpenWeather API. The user can search for a city, and the app displays accurate and up-to-date weather information in a clean and user-friendly design.</div>
+//             <div className='flex gap-5'>
+//             <img src="./src/assets/img/html.webp" className="w-[35px] h-[35px]" />
+//                   <img src="./src/assets/img/css.svg" className="w-[35px] h-[35px]" />
+//                   <img src="./src/assets/img/js.png" className="w-[35px] h-[35px]" />
+//                   <img src="./src/assets/img/api.svg" className="w-[35px] h-[35px]" />
+//             </div>
+//             <div className='flex gap-2 items-end justify-end'>
+//            <a href="https://github.com/RahulQurbanov/Weather-prognoz"><img src="./src/assets/img/github1.svg" className="w-[50px] h-[50px] cursor-pointer  opacity-45 hover:opacity-100" /></a>
+//             <a href="https://rahulqurbanov.github.io/Weather-prognoz/"><img src="./src/assets/img/live.svg" className="w-[40px] h-[40px] cursor-pointer opacity-45 hover:opacity-100" /></a>
+//             </div>
+//             </div>
+//             <div className='w-[50%] flex justify-end'>
+//             <a href="https://github.com/RahulQurbanov/Weather-prognoz"><img src="src/assets/img/Weather.png" className='h-[97%] w-[485px] rounded-lg opacity-20 hover:opacity-65 cursor-pointer transition-all translate-0.5 ease-in-out'/></a>
+//             </div>
+//         </div>
+<div className='flex flex-row gap-[20px] justify-around'>
+//             <div className='w-[45%] flex flex-col gap-[15px]'>
+//                <h2 className='text-[#ccd6f6] text-[30px] font-bold'>Weather Projects</h2>
+//                <div className='w-[500px] bg-[#112240] text-gray-300 rounded-lg py-2 px-5'>This project was built using HTML, CSS, and JavaScript. It fetches real-time 7-day weather forecasts for any city worldwide using the OpenWeather API. The user can search for a city, and the app displays accurate and up-to-date weather information in a clean and user-friendly design.</div>
+//             <div className='flex gap-5'>
+//             <img src="./src/assets/img/html.webp" className="w-[35px] h-[35px]" />
+//                   <img src="./src/assets/img/css.svg" className="w-[35px] h-[35px]" />
+//                   <img src="./src/assets/img/js.png" className="w-[35px] h-[35px]" />
+//                   <img src="./src/assets/img/api.svg" className="w-[35px] h-[35px]" />
+//             </div>
+//             <div className='flex gap-2 items-end justify-end'>
+//            <a href="https://github.com/RahulQurbanov/Weather-prognoz"><img src="./src/assets/img/github1.svg" className="w-[50px] h-[50px] cursor-pointer  opacity-45 hover:opacity-100" /></a>
+//             <a href="https://rahulqurbanov.github.io/Weather-prognoz/"><img src="./src/assets/img/live.svg" className="w-[40px] h-[40px] cursor-pointer opacity-45 hover:opacity-100" /></a>
+//             </div>
+//             </div>
+//             <div className='w-[50%] flex justify-end'>
+//             <a href="https://github.com/RahulQurbanov/Weather-prognoz"><img src="src/assets/img/Weather.png" className='h-[97%] w-[485px] rounded-lg opacity-20 hover:opacity-65 cursor-pointer transition-all translate-0.5 ease-in-out'/></a>
+//             </div>
+//         </div>
       </div>
     </div>
   );
 }
-
