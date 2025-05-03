aed10baae6f215af252745da2a738709f5019d4e
Rahul Qurbanov <rahulqurbanov055@gmail.com>
2025-05-03 21:16:51 +0400

    Add Animation

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -19,7 +19,7 @@ export default function Projects() {
           Profile.
         </h1>
         
-        <div className='flex flex-row gap-[20px] justify-around mb-[125px' ref={projectRef}>
+        <div className='flex flex-row gap-[20px] justify-around mb-[95px]' ref={projectRef}>
           <div className={`w-[50%] ${inView ? 'animate__animated animate__zoomIn' : ''}`}>
             <a href="https://github.com/RahulQurbanov/Investment-Calculator">
               <img src="src/assets/img/investment.png" className='h-[75%] w-[475px] rounded-xl opacity-20 hover:opacity-65 cursor-pointer transition-all translate-0.5 ease-in-out' />
@@ -46,7 +46,7 @@ export default function Projects() {
             </div>
           </div>
         </div>
-        <div className='flex flex-row gap-[20px] justify-between mb-[105px' ref={projectRef}>
+        <div className='flex flex-row gap-[20px] justify-between mb-[95px]' ref={projectRef}>
             <div className={`w-[50%] flex flex-col gap-[15px]  ${inView ? 'animate__animated animate__zoomIn' : ''}`}>
                <h2 className='text-[#ccd6f6] text-[30px] font-bold'>Investment Calculator</h2>
                <div className='w-[500x] bg-[#112240] text-gray-300 rounded-lg py-2 px-5'>This project is an interactive Investment Calculator built with React and styled using Tailwind CSS.
@@ -68,11 +68,11 @@ Users can input their desired investment amount, interest rate, and the number o
             <a href="https://github.com/RahulQurbanov/Investment-Calculator"><img src="src/assets/img/investment.png" className='h-[75%] w-[475px] rounded-xl opacity-20 hover:opacity-65 cursor-pointer transition-all translate-0.5 ease-in-out'/></a>
             </div>
         </div>
-        <div className='flex flex-row gap-[20px] justify-around mb-[105px'>
-        <div className='w-[50%]'>
+        <div className='flex flex-row gap-[20px] justify-around mb-[95px]' ref={projectRef}>
+        <div className={`w-[50%] ${inView ? 'animate__animated animate__zoomIn' : ''}`}>
             <a href="https://github.com/RahulQurbanov/Quiz-App"><img src="src/assets/img/quizapp.png" className='h-[95%] w-[480px] rounded-lg opacity-20 hover:opacity-65 cursor-pointer transition-all translate-0.5 ease-in-out'/></a>
             </div>
-            <div className='w-[45%] flex flex-col gap-[15px]'>
+            <div className={`w-[45%] flex flex-col gap-[15px] ${inView ? 'animate__animated animate__zoomIn' : ''}`}>
                <h2 className='text-[#ccd6f6] text-[30px] font-bold'>Quiz App</h2>
                <div className='w-[500px] bg-[#112240] text-gray-300 rounded-lg py-2 px-5'>This is a dynamic Quiz App built using React and styled with Tailwind CSS.
                 All questions are loaded from a JSON file, and users receive points for each correct answer, while wrong answers do not reduce the score. The app offers smooth interaction, real-time feedback, and is perfect for quick knowledge testing.</div>
@@ -89,8 +89,8 @@ Users can input their desired investment amount, interest rate, and the number o
             </div>
             </div>
         </div>
-        <div className='flex flex-row gap-[20px] justify-around mb-[105px'>
-            <div className='w-[45%] flex flex-col gap-[15px]'>
+        <div className='flex flex-row gap-[20px] justify-around mb-[95px]' ref={projectRef}>
+            <div className={`w-[45%] flex flex-col gap-[15px]  ${inView ? 'animate__animated animate__zoomIn' : ''}`}>
                <h2 className='text-[#ccd6f6] text-[30px] font-bold'>TodoList App</h2>
                <div className='w-[500px] bg-[#112240] text-gray-300 rounded-lg py-2 px-5'>This project is a To-Do List Application built with React and Vite. The application features two separate sections: Active and Done — allowing users to efficiently organize their tasks. Users can easily add new tasks, edit task names, mark them as completed, or delete them. The app offers a smooth and intuitive experience, making task management clear and convenient.</div>
             <div className='flex gap-5'>
@@ -104,15 +104,15 @@ Users can input their desired investment amount, interest rate, and the number o
             <a href="https://rahulqurbanov.github.io/Todo-List-/"><img src="./src/assets/img/live.svg" className="w-[40px] h-[40px] cursor-pointer opacity-45 hover:opacity-100" /></a>
             </div>
             </div>
-            <div className='w-[50%] flex justify-end'>
+            <div className={`w-[50%] flex justify-end ${inView ? 'animate__animated animate__zoomIn' : ''}`}>
             <a href="https://rahulqurbanov.github.io/Todo-List-/"><img src="src/assets/img/todo-react.png" className='h-[85%] mt-[15px] w-[465px] rounded-lg opacity-20 hover:opacity-65 cursor-pointer transition-all translate-0.5 ease-in-out'/></a>
             </div>
         </div>
-        <div className='flex flex-row gap-[20px] justify-around mb-[105px]'>
-        <div className='w-[50%]'>
+        <div className='flex flex-row gap-[20px] justify-around mb-[95px]' ref={projectRef}>
+        <div className={`w-[50%] ${inView ? 'animate__animated animate__zoomIn' : ''}`}>
             <a href="https://github.com/RahulQurbanov/Project-KoolCoders"><img src="src/assets/img/KoolCoders.png" className='h-[95%] w-[475px] rounded-lg opacity-20 hover:opacity-65 cursor-pointer transition-all translate-0.5 ease-in-out'/></a>
             </div>
-            <div className='w-[45%] flex flex-col gap-[15px]'>
+            <div className={`w-[45%] flex flex-col gap-[15px] ${inView ? 'animate__animated animate__zoomIn' : ''}`}>
                <h2 className='text-[#ccd6f6] text-[30px] font-bold'>KoolCoders Projects</h2>
                <div className='w-[500px] bg-[#112240] text-gray-300 rounded-lg py-2 px-5'>This project is designed as an online course platform. It was fully built using HTML, CSS, and JavaScript. The website layout is clean and responsive, featuring sections for courses, instructors, and interactive elements to simulate a real e-learning experience.</div>
             <div className='flex gap-5'>
@@ -127,7 +127,7 @@ Users can input their desired investment amount, interest rate, and the number o
             </div>
         </div>
         <div className='flex flex-row gap-[20px] justify-around'>
-            <div className='w-[45%] flex flex-col gap-[15px]'>
+            <div className={`w-[45%] flex flex-col gap-[15px]  ${inView ? 'animate__animated animate__zoomIn' : ''}`}>
                <h2 className='text-[#ccd6f6] text-[30px] font-bold'>Weather Projects</h2>
                <div className='w-[500px] bg-[#112240] text-gray-300 rounded-lg py-2 px-5'>This project was built using HTML, CSS, and JavaScript. It fetches real-time 7-day weather forecasts for any city worldwide using the OpenWeather API. The user can search for a city, and the app displays accurate and up-to-date weather information in a clean and user-friendly design.</div>
             <div className='flex gap-5'>
@@ -141,7 +141,7 @@ Users can input their desired investment amount, interest rate, and the number o
             <a href="https://rahulqurbanov.github.io/Weather-prognoz/"><img src="./src/assets/img/live.svg" className="w-[40px] h-[40px] cursor-pointer opacity-45 hover:opacity-100" /></a>
             </div>
             </div>
-            <div className='w-[50%] flex justify-end'>
+            <div className={`w-[50%] flex justify-end ${inView ? 'animate__animated animate__zoomIn' : ''}`}>
             <a href="https://github.com/RahulQurbanov/Weather-prognoz"><img src="src/assets/img/Weather.png" className='h-[97%] w-[485px] rounded-lg opacity-20 hover:opacity-65 cursor-pointer transition-all translate-0.5 ease-in-out'/></a>
             </div>
         </div>
