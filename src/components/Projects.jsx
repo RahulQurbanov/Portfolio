dcd1f1d00f5dbf80d6090a81d635dded66b31036
Rahul Qurbanov <rahulqurbanov055@gmail.com>
2025-05-05 21:20:27 +0400

    Some projects changed animation

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -243,6 +243,46 @@ Users can input their desired investment amount, interest rate, and the number o
             <a href="https://github.com/RahulQurbanov/Investment-Calculator"><img src="src/assets/img/investment.png" className='h-[75%] w-[475px] rounded-xl opacity-20 hover:opacity-65 cursor-pointer transition-all translate-0.5 ease-in-out'/></a>
             </div>
         </div>
+        <div className='flex flex-row gap-[20px] justify-around mb-[45px]' ref={quizRef}>
+        <div className={`w-[50%]`}>
+            <a href="https://github.com/RahulQurbanov/Quiz-App"><img src="src/assets/img/quizapp.png" className='h-[95%] w-[480px] rounded-lg opacity-20 hover:opacity-65 cursor-pointer transition-all translate-0.5 ease-in-out'/></a>
+            </div>
+            <div className={`w-[45%] flex flex-col gap-[15px]`}>
+               <h2 className='text-[#ccd6f6] text-[30px] font-bold'>Quiz App</h2>
+               <div className='w-[500px] bg-[#112240] text-gray-300 rounded-lg py-2 px-5'>This is a dynamic Quiz App built using React and styled with Tailwind CSS.
+                All questions are loaded from a JSON file, and users receive points for each correct answer, while wrong answers do not reduce the score. The app offers smooth interaction, real-time feedback, and is perfect for quick knowledge testing.</div>
+            <div className='flex gap-5'>
+                  <img src="./src/assets/img/html.webp" className={`w-[35px] h-[35px] ${quizInView ? 'animate__animated animate__rotateIn' : ''}`} />
+                  <img src="./src/assets/img/js.png" className={`w-[35px] h-[35px] ${quizInView ? 'animate__animated animate__rotateIn' : ''}`} />
+                  <img src="./src/assets/img/react.svg" className={`w-[35px] h-[35px] ${quizInView ? 'animate__animated animate__rotateIn' : ''}`} />
+                  <img src="./src/assets/img/vite.svg" className={`w-[35px] h-[35px] ${quizInView ? 'animate__animated animate__rotateIn' : ''}`} />
+                  <img src="./src/assets/img/tailwind.svg" className={`w-[35px] h-[35px] ${quizInView ? 'animate__animated animate__rotateIn' : ''}`} />
+            </div>
+            <div className='flex gap-2 items-end justify-end'>
+           <a href="https://github.com/RahulQurbanov/Quiz-App"><img src="./src/assets/img/github1.svg" className="w-[50px] h-[50px] cursor-pointer  opacity-45 hover:opacity-100" /></a>
+            <a href="http://localhost:5173/"><img src="./src/assets/img/live.svg" className="w-[40px] bg-gradient-to-r from-pink-500 via-purple-500 to-blue-500 bg-clip-text text-transparent animate-pulse h-[40px] cursor-pointer" /></a>
+            </div>
+            </div>
+        </div>
+        <div className='flex flex-row gap-[20px] justify-around mb-[95px]' ref={todoRef}>
+            <div className={`w-[45%] flex flex-col gap-[15px]`}>
+               <h2 className='text-[#ccd6f6] text-[30px] font-bold'>TodoList App</h2>
+               <div className='w-[500px] bg-[#112240] text-gray-300 rounded-lg py-2 px-5'>This project is a To-Do List Application built with React and Vite. The application features two separate sections: Active and Done — allowing users to efficiently organize their tasks. Users can easily add new tasks, edit task names, mark them as completed, or delete them. The app offers a smooth and intuitive experience, making task management clear and convenient.</div>
+            <div className='flex gap-5'>
+            <img src="./src/assets/img/html.webp" className={`w-[35px] h-[35px] ${todoInView ? 'animate__animated animate__rotateIn' : ''}`} />
+                  <img src="./src/assets/img/js.png" className={`w-[35px] h-[35px] ${todoInView ? 'animate__animated animate__rotateIn' : ''}`} />
+                  <img src="./src/assets/img/react.svg" className={`w-[35px] h-[35px] ${todoInView ? 'animate__animated animate__rotateIn' : ''}`} />
+                  <img src="./src/assets/img/vite.svg" className={`w-[35px] h-[35px] ${todoInView ? 'animate__animated animate__rotateIn' : ''}`} />
+            </div>
+            <div className='flex gap-2 items-end justify-end'>
+           <a href="https://github.com/RahulQurbanov/Todo-List-"><img src="./src/assets/img/github1.svg" className="w-[50px] h-[50px] cursor-pointer  opacity-45 hover:opacity-100" /></a>
+            <a href="https://rahulqurbanov.github.io/Todo-List-/"><img src="./src/assets/img/live.svg" className="w-[40px] bg-gradient-to-r from-pink-500 via-purple-500 to-blue-500 bg-clip-text text-transparent animate-pulse h-[40px] cursor-pointer" /></a>
+            </div>
+            </div>
+            <div className={`w-[50%] flex justify-end`}>
+            <a href="https://rahulqurbanov.github.io/Todo-List-/"><img src="src/assets/img/todo-react.png" className='h-[85%] mt-[15px] w-[465px] rounded-lg opacity-20 hover:opacity-65 cursor-pointer transition-all translate-0.5 ease-in-out'/></a>
+            </div>
+        </div>
       </div>
       </div>
   )}
