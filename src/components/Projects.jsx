acd66499c819bd0fab1a72cc653350baae3304e7
Rahul Qurbanov <rahulqurbanov055@gmail.com>
2025-05-06 18:35:36 +0400

    Last change img path in Project Part

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -117,35 +117,35 @@ Users can input their desired investment amount, interest rate, and the number o
                <h2 className='text-[#ccd6f6] text-[30px] font-bold'>TodoList App</h2>
                <div className='w-[500px] bg-[#112240] text-gray-300 rounded-lg py-2 px-5'>This project is a To-Do List Application built with React and Vite. The application features two separate sections: Active and Done — allowing users to efficiently organize their tasks. Users can easily add new tasks, edit task names, mark them as completed, or delete them. The app offers a smooth and intuitive experience, making task management clear and convenient.</div>
             <div className='flex gap-5'>
-            <img src="./src/assets/img/html.webp" className={`w-[35px] h-[35px] ${todoInView ? 'animate__animated animate__rotateIn' : ''}`} />
-                  <img src="./src/assets/img/js.png" className={`w-[35px] h-[35px] ${todoInView ? 'animate__animated animate__rotateIn' : ''}`} />
-                  <img src="./src/assets/img/react.svg" className={`w-[35px] h-[35px] ${todoInView ? 'animate__animated animate__rotateIn' : ''}`} />
-                  <img src="./src/assets/img/vite.svg" className={`w-[35px] h-[35px] ${todoInView ? 'animate__animated animate__rotateIn' : ''}`} />
+            <img src="/img/html.webp" className={`w-[35px] h-[35px] ${todoInView ? 'animate__animated animate__rotateIn' : ''}`} />
+                  <img src="/img/js.png" className={`w-[35px] h-[35px] ${todoInView ? 'animate__animated animate__rotateIn' : ''}`} />
+                  <img src="/img/react.svg" className={`w-[35px] h-[35px] ${todoInView ? 'animate__animated animate__rotateIn' : ''}`} />
+                  <img src="/img/vite.svg" className={`w-[35px] h-[35px] ${todoInView ? 'animate__animated animate__rotateIn' : ''}`} />
             </div>
             <div className='flex gap-2 items-end justify-end'>
-           <a href="https://github.com/RahulQurbanov/Todo-List-"><img src="./src/assets/img/github1.svg" className="w-[50px] h-[50px] cursor-pointer  opacity-45 hover:opacity-100" /></a>
-            <a href="https://rahulqurbanov.github.io/Todo-List-/"><img src="./src/assets/img/live.svg" className="w-[40px] bg-gradient-to-r from-pink-500 via-purple-500 to-blue-500 bg-clip-text text-transparent animate-pulse h-[40px] cursor-pointer" /></a>
+           <a href="https://github.com/RahulQurbanov/Todo-List-"><img src="/img/github1.svg" className="w-[50px] h-[50px] cursor-pointer  opacity-45 hover:opacity-100" /></a>
+            <a href="https://rahulqurbanov.github.io/Todo-List-/"><img src="/img/live.svg" className="w-[40px] bg-gradient-to-r from-pink-500 via-purple-500 to-blue-500 bg-clip-text text-transparent animate-pulse h-[40px] cursor-pointer" /></a>
             </div>
             </div>
             <div className={`w-[50%] flex justify-end`}>
-            <a href="https://rahulqurbanov.github.io/Todo-List-/"><img src="src/assets/img/todo-react.png" className='h-[85%] mt-[15px] w-[465px] rounded-lg opacity-20 hover:opacity-65 cursor-pointer transition-all translate-0.5 ease-in-out'/></a>
+            <a href="https://rahulqurbanov.github.io/Todo-List-/"><img src="/img/todo-react.png" className='h-[85%] mt-[15px] w-[465px] rounded-lg opacity-20 hover:opacity-65 cursor-pointer transition-all translate-0.5 ease-in-out'/></a>
             </div>
         </div>
         <div className='flex flex-row gap-[20px] justify-around mb-[45px]' ref={koolCodersRef}>
         <div className={`w-[50%]`}>
-            <a href="https://github.com/RahulQurbanov/Project-KoolCoders"><img src="src/assets/img/KoolCoders.png" className='h-[95%] w-[475px] rounded-lg opacity-20 hover:opacity-65 cursor-pointer transition-all translate-0.5 ease-in-out'/></a>
+            <a href="https://github.com/RahulQurbanov/Project-KoolCoders"><img src="/img/KoolCoders.png" className='h-[95%] w-[475px] rounded-lg opacity-20 hover:opacity-65 cursor-pointer transition-all translate-0.5 ease-in-out'/></a>
             </div>
             <div className={`w-[45%] flex flex-col gap-[15px]`}>
                <h2 className='text-[#ccd6f6] text-[30px] font-bold'>KoolCoders Projects</h2>
                <div className='w-[500px] bg-[#112240] text-gray-300 rounded-lg py-2 px-5'>This project is designed as an online course platform. It was fully built using HTML, CSS, and JavaScript. The website layout is clean and responsive, featuring sections for courses, instructors, and interactive elements to simulate a real e-learning experience.</div>
             <div className='flex gap-5'>
-            <img src="./src/assets/img/html.webp" className={`w-[35px] h-[35px] ${koolCodersInView ? 'animate__animated animate__rotateIn' : ''}`} />
-                  <img src="./src/assets/img/css.svg" className={`w-[35px] h-[35px] ${koolCodersInView ? 'animate__animated animate__rotateIn' : ''}`} />
-                  <img src="./src/assets/img/js.png" className={`w-[35px] h-[35px] ${koolCodersInView ? 'animate__animated animate__rotateIn' : ''}`} />
+            <img src="/img/html.webp" className={`w-[35px] h-[35px] ${koolCodersInView ? 'animate__animated animate__rotateIn' : ''}`} />
+                  <img src="/img/css.svg" className={`w-[35px] h-[35px] ${koolCodersInView ? 'animate__animated animate__rotateIn' : ''}`} />
+                  <img src="/img/js.png" className={`w-[35px] h-[35px] ${koolCodersInView ? 'animate__animated animate__rotateIn' : ''}`} />
             </div>
             <div className='flex gap-2 items-end justify-end'>
-           <a href="https://github.com/RahulQurbanov/Project-KoolCoders"><img src="./src/assets/img/github1.svg" className="w-[50px] h-[50px] cursor-pointer  opacity-45 hover:opacity-100" /></a>
-            <a href="https://rahulqurbanov.github.io/Project-KoolCoders/"><img src="./src/assets/img/live.svg" className="w-[40px] bg-gradient-to-r from-pink-500 via-purple-500 to-blue-500 bg-clip-text text-transparent animate-pulse h-[40px] cursor-pointer" /></a>
+           <a href="https://github.com/RahulQurbanov/Project-KoolCoders"><img src="/img/github1.svg" className="w-[50px] h-[50px] cursor-pointer  opacity-45 hover:opacity-100" /></a>
+            <a href="https://rahulqurbanov.github.io/Project-KoolCoders/"><img src="/img/live.svg" className="w-[40px] bg-gradient-to-r from-pink-500 via-purple-500 to-blue-500 bg-clip-text text-transparent animate-pulse h-[40px] cursor-pointer" /></a>
             </div>
             </div>
         </div>
@@ -154,18 +154,18 @@ Users can input their desired investment amount, interest rate, and the number o
                <h2 className='text-[#ccd6f6] text-[30px] font-bold'>Weather Projects</h2>
                <div className='w-[500px] bg-[#112240] text-gray-300 rounded-lg py-2 px-5'>This project was built using HTML, CSS, and JavaScript. It fetches real-time 7-day weather forecasts for any city worldwide using the OpenWeather API. The user can search for a city, and the app displays accurate and up-to-date weather information in a clean and user-friendly design.</div>
             <div className='flex gap-5'>
-            <img src="./src/assets/img/html.webp" className={`w-[35px] h-[35px] ${weatherInView ? 'animate__animated animate__rotateIn' : ''}`} />
-                  <img src="./src/assets/img/css.svg" className={`w-[35px] h-[35px] ${weatherInView ? 'animate__animated animate__rotateIn' : ''}`} />
-                  <img src="./src/assets/img/js.png" className={`w-[35px] h-[35px] ${weatherInView ? 'animate__animated animate__rotateIn' : ''}`} />
-                  <img src="./src/assets/img/api.svg" className={`w-[35px] h-[35px] ${weatherInView ? 'animate__animated animate__rotateIn' : ''}`} />
+            <img src="/img/html.webp" className={`w-[35px] h-[35px] ${weatherInView ? 'animate__animated animate__rotateIn' : ''}`} />
+                  <img src="/img/css.svg" className={`w-[35px] h-[35px] ${weatherInView ? 'animate__animated animate__rotateIn' : ''}`} />
+                  <img src="/img/js.png" className={`w-[35px] h-[35px] ${weatherInView ? 'animate__animated animate__rotateIn' : ''}`} />
+                  <img src="/img/api.svg" className={`w-[35px] h-[35px] ${weatherInView ? 'animate__animated animate__rotateIn' : ''}`} />
             </div>
             <div className='flex gap-2 items-end justify-end'>
-           <a href="https://github.com/RahulQurbanov/Weather-prognoz"><img src="./src/assets/img/github1.svg" className="w-[50px] h-[50px] cursor-pointer  opacity-45 hover:opacity-100" /></a>
-            <a href="https://rahulqurbanov.github.io/Weather-prognoz/"><img src="./src/assets/img/live.svg" className="w-[40px] bg-gradient-to-r from-pink-500 via-purple-500 to-blue-500 bg-clip-text text-transparent animate-pulse h-[40px] cursor-pointer" /></a>
+           <a href="https://github.com/RahulQurbanov/Weather-prognoz"><img src="/img/github1.svg" className="w-[50px] h-[50px] cursor-pointer  opacity-45 hover:opacity-100" /></a>
+            <a href="https://rahulqurbanov.github.io/Weather-prognoz/"><img src="/img/live.svg" className="w-[40px] bg-gradient-to-r from-pink-500 via-purple-500 to-blue-500 bg-clip-text text-transparent animate-pulse h-[40px] cursor-pointer" /></a>
             </div>
             </div>
             <div className={`w-[50%] flex justify-end`}>
-            <a href="https://github.com/RahulQurbanov/Weather-prognoz"><img src="src/assets/img/Weather.png" className='h-[97%] w-[485px] rounded-lg opacity-20 hover:opacity-65 cursor-pointer transition-all translate-0.5 ease-in-out'/></a>
+            <a href="https://github.com/RahulQurbanov/Weather-prognoz"><img src="/img/Weather.png" className='h-[97%] w-[485px] rounded-lg opacity-20 hover:opacity-65 cursor-pointer transition-all translate-0.5 ease-in-out'/></a>
             </div>
         </div>
       </div>
