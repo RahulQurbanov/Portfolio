5f0facc32998512b3a8390d4e328accd1fbe0900
RahulQurbanov <156121142+RahulQurbanov@users.noreply.github.com>
2025-05-09 20:39:49 +0400

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
-// import Header from './components/Header'
+import Header from './components/Header'
 import Projects from './components/Projects'
-// import Footer from './components/Footer'
+import Footer from './components/Footer'
 
 export default function App() {
   return (<>
