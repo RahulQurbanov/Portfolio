68e10de44c3be0b1d5345ae4adb1c1b52db623a8
Rahul Qurbanov <rahulqurbanov055@gmail.com>
2025-05-17 20:02:22 +0400

    Responsive Footer Part

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ export default function App() {
     
    <Header/>
    <Projects/>
-   {/* <Footer/> */}
+   <Footer/>
    </>
   )
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,12 +3,15 @@ import React from "react";
 export default function Footer() {
   return (
     <div className="bg-[#0a192f]">
-      <div id="contact" className="w-[85%] max-w-6xl mx-auto pt-[75px]">
-        <h1 className="text-white text-3xl font-bold mb-6 border-b-2 border-[#133563] mt-2 inline-block">
+      <div
+        id="contact"
+        className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 pt-16"
+      >
+        <h1 className="text-white text-3xl font-bold mb-6 border-b-2 border-[#133563] inline-block">
           📬 Contact Info
         </h1>
 
-        <div className="text-white text-lg mt-2 ">
+        <div className="text-white text-lg space-y-3 mt-2">
           <p>
             📧 Email:{" "}
             <span className="text-purple-300 font-semibold">
@@ -17,36 +20,40 @@ export default function Footer() {
           </p>
           <p>
             📞 Phone:{" "}
-            <span className="text-purple-300 font-semibold mt-1">
+            <span className="text-purple-300 font-semibold">
               +9940558902705
             </span>
           </p>
         </div>
 
-        <div className="flex gap-4 items-center mt-6">
+        <div className="flex gap-6 items-center mt-6">
           <a
             href="https://github.com/RahulQurbanov"
             className="transform hover:scale-110 transition duration-200"
+            target="_blank"
+            rel="noopener noreferrer"
           >
             <img
               src="/img/github.svg"
-              className="w-[35px] h-[35px] cursor-pointer"
               alt="GitHub"
+              className="w-8 h-8 sm:w-9 sm:h-9 cursor-pointer"
             />
           </a>
           <a
             href="https://www.linkedin.com/in/rahul-qurbanov-483148261/"
             className="transform hover:scale-110 transition duration-200"
+            target="_blank"
+            rel="noopener noreferrer"
           >
             <img
               src="/img/linkedin.svg"
-              className="w-[30px] h-[30px] cursor-pointer"
               alt="LinkedIn"
+              className="w-7 h-7 sm:w-8 sm:h-8 cursor-pointer"
             />
           </a>
         </div>
 
-        <div className="mt-5 border-t border-[#133563] pt-4 text-center text-sm text-purple-200">
+        <div className="mt-8 border-t border-[#133563] pt-4 text-center text-sm text-purple-200">
           © {new Date().getFullYear()} Rahul Qurbanov. All rights reserved.
         </div>
       </div>
